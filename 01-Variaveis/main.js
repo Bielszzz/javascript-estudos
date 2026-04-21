5b828f60a793dd66f8e90c94cccfb374958a3ede
Gabriel <gabrieloliveira0099@gmail.com>
2026-04-21 14:55:40 -0300

    feat: adiciona estudo de variveis

---
+++ 01-Variaveis/main.js
@@ -0,0 +1,37 @@
+// declaracao de variaveis -- var ; const ; let 
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String#static_methods
+
+// Strings
+const message = 'Hello World!'
+
+alert(message)
+
+c = message.length
+console.log(`message tem ${c} caracteres!`)
+
+const names = 'Felipe,Joao,Gabriel'
+
+console.log(names.split(','))
+
+// Numbers
+let number = 5
+
+number += 2
+
+console.log(number)
+
+// Booleanos 
+false 
+true 
+
+console.log(2 == 3)
+
+// Null e Undefined 
+const x = null 
+
+const y = undefined
+
+// List e Object
+const list = [1, 2, 3, 4, 5]
+
+const Object = {nome: 'Gabriel'}
